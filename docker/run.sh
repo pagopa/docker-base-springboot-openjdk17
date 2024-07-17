94365a7db61abc9c3b4567669264facd040697f3
Angelo Caporaso <56113767+AngeloCaporaso@users.noreply.github.com>
2024-07-17 17:21:52 +0200

    feat: Add SpringBoot 3 JarLauncher

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-exec java -javaagent:/applicationinsights-agent.jar ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher "$@"
+version=$(cat META-INF/MANIFEST.MF | grep -oP 'Spring-Boot-Version: \K[\d.]+')
+version_major=${version:0:1}
+
+# Check if the version_major is 3
+if [[ "$version_major" == "3" ]]; then
+   exec java -javaagent:/applicationinsights-agent.jar ${JAVA_OPTS} org.springframework.boot.loader.launch.JarLauncher "$@"
+else
+   exec java -javaagent:/applicationinsights-agent.jar ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher "$@"
+fi
